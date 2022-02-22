b0a09bd1091bbf0802b54835f1287c6963bab443
silviapeng <silviap3ng@gmail.com>
2022-02-22 09:55:57 +0800

    update User

---
+++ app/src/main/java/com/example/carpoolbuddypro/silvia/Teacher.java
@@ -0,0 +1,32 @@
+package com.example.carpoolbuddypro.silvia;
+
+import java.util.ArrayList;
+
+public class Teacher extends User
+{
+    String inschooltitle;
+
+    public Teacher ()
+    {
+    }
+
+    public String getInschooltitle() {
+        return inschooltitle;
+    }
+
+    public void setInschooltitle(String inschooltitle) {
+        this.inschooltitle = inschooltitle;
+    }
+
+    public Teacher(String uid, String name, String email, String usertype, double pricemult, ArrayList<String> ownveh, double balance, String inschooltitle) {
+        super(uid, name, email, usertype, pricemult, ownveh, balance);
+        this.inschooltitle = inschooltitle;
+    }
+
+    @Override
+    public String toString() {
+        return "teacher{" +
+                "inschooltitle='" + inschooltitle + '\'' +
+                '}';
+    }
+}
