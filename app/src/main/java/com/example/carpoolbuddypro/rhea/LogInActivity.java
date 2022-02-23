a0eede17df99dac371fdca846fbd59ea9f1d9d0b
rtalwar05 <talwarrhea@gmail.com>
2022-02-23 13:47:13 +0800

    connected Firebase and created LogInActivity

---
+++ app/src/main/java/com/example/carpoolbuddypro/rhea/LogInActivity.java
@@ -0,0 +1,31 @@
+package com.example.carpoolbuddypro.rhea;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.carpoolbuddypro.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class LogInActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+    }
+
+
+
+
+
+}
