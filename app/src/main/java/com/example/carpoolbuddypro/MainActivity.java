8a9d786a1ae466788fd346a8694ca5359b3678b8
pset1 (version 1, unfinished) <audreychan2005@gmail.com>
2022-02-23 11:44:20 +0800

    this is just for testing

--- app/src/main/java/com/example/carpoolbuddypro/MainActivity.java
+++ app/src/main/java/com/example/carpoolbuddypro/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String example;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
