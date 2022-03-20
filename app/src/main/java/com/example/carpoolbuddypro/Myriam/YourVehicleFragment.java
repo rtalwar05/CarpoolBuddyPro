2fc47fc7c1a02c397c5b9a01a1c16fca1880e92e
myriamlynch <myriaml2023@student.cis.edu.hk>
2022-03-20 23:05:55 +0800

    updated nav bar and added comments

--- app/src/main/java/com/example/carpoolbuddypro/Myriam/YourVehicleFragment.java
+++ app/src/main/java/com/example/carpoolbuddypro/Myriam/YourVehicleFragment.java
@@ -12,6 +12,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.carpoolbuddypro.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -21,6 +27,14 @@ public class YourVehicleFragment extends Fragment
     View root;
 
     ArrayList<String> test;
+    ArrayList<String> name;
+    ArrayList<String> cap;
+
+    private FirebaseFirestore firestore;
+    private FirebaseAuth mAuth;
+
+    MyCarAdapter myAdapter;
+
 
     RecyclerView recView1;
 
@@ -31,18 +45,69 @@ public class YourVehicleFragment extends Fragment
 
         recView1 = root.findViewById(R.id.recView1);
 
+        name = new ArrayList<String>();
+        cap = new ArrayList<String>();
+
+        firestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
         test = new ArrayList<>();
         test.add("myriam");
         test.add("audrey");
         test.add("rhea");
         test.add("silvia");
-        MyCarAdapter myAdapter = new MyCarAdapter(test);
+        myAdapter = new MyCarAdapter(name, cap);
         recView1.setAdapter(myAdapter);
         recView1.setLayoutManager(new LinearLayoutManager(root.getContext()));
 
         System.out.println("adapter" + recView1.getAdapter());
 
-        return inflater.inflate(R.layout.fragment_your_vehicle,container,false);
+        getAndPopulateData();
 
+        return root;
+
+    }
+
+    public void getAndPopulateData()
+    {
+        name = new ArrayList<String>();
+        cap = new ArrayList<String>();
+
+
+        //get information from firebase
+        //DO: get for specific user
+        firestore.collection("vehicles").get().addOnCompleteListener
+                (new OnCompleteListener<QuerySnapshot>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (DocumentSnapshot ds : task.getResult().getDocuments())
+                        {
+                            String result = "" + ds;
+                            int cIntStart = result.indexOf("capacity");
+                            int cIntEnd = result.indexOf("open");
+                            int nIntStart = result.indexOf("owner");
+                            int nIntEnd = result.length();
+
+                            System.out.println(result);
+
+                            String names = result.substring(cIntStart + 9, cIntEnd - 1);
+                            System.out.println(names);
+                            String capa = result.substring(nIntStart + 6, nIntEnd - 4);
+                            System.out.println(capa);
+
+
+                            //if the vehicle is open, save information in arraylist for recyclerview
+                            cap.add(capa);
+                            name.add(names);
+
+
+
+                            //update recyclerview
+                            myAdapter.setData(name, cap);
+                            myAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
     }
 }
